eeecadba628a933d1df3afc360cf2ba38f46e01c
Rashidkiplagat <rashidkiplagat67@gmail.com>
2023-03-16 16:05:40 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main- the main funcion
+*
+* Return: alwayws o (success)
+*/
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
