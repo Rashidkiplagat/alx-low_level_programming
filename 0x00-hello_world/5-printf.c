7c717a8112bbe6394776bbc254b76c855ff559f7
Rashidkiplagat <rashidkiplagat67@gmail.com>
2023-03-16 16:17:10 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
 * Return: alwayws o (success)
 */
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,");
+{printf("with proper grammar, but the outcome is a piece of art,.");
 return (0);
 }
